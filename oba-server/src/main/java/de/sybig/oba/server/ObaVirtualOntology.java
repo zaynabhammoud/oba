8455580ab883814328269b03eb01df7c682f993d
zaynabhammoud <zaynabhassanhammoud@gmail.com>
2016-07-28 10:46:44 +0200

    Add new Methods.

--- oba-server/src/main/java/de/sybig/oba/server/ObaVirtualOntology.java
+++ oba-server/src/main/java/de/sybig/oba/server/ObaVirtualOntology.java
@@ -6,39 +6,55 @@
 package de.sybig.oba.server;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Set;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.store.LockObtainFailedException;
+import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
+import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 /**
  *
  * @author zaynab
  */
-public class ObaVirtualOntology extends ObaOntology{
-    OntologyResource ontA,ontB;
-    
+public class ObaVirtualOntology extends ObaOntology {
+
+    OntologyResource ontA, ontB;
+
     /**
-	 * Sets the ontology used in iteration.
-         * We consider that the OntA is one that we will iterate over it.
-	 * 
-	 * @param ontology
-	 */
-    public void setOntA(OntologyResource onto){
-        ontA=onto;
+     * Sets the ontology used in iteration. We consider that the OntA is one
+     * that we will iterate over it.
+     *
+     * @param ontology
+     */
+    public void setOntA(OntologyResource onto) {
+        ontA = onto;
     }
+
     /**
-	 * Sets the ontology used in comparison.
-	 * The elements of this ontology will be compared and mapped to OntA.
-         * 
-	 * @param ontology
-	 */
-    public void setOntB(OntologyResource onto){
-        ontB=onto;
+     * Sets the ontology used in comparison. The elements of this ontology will
+     * be compared and mapped to OntA.
+     *
+     * @param ontology
+     */
+    public void setOntB(OntologyResource onto) {
+        ontB = onto;
     }
-    
-        /**
+
+    public OntologyResource getOntA() {
+        return ontA;
+    }
+
+    public OntologyResource getOntB() {
+        return ontB;
+    }
+
+    /**
      * Get the root node of the ontology. The root is returned as proxy from
      * Type ObaClass with the ontology set.
      *
@@ -48,22 +64,33 @@ public class ObaVirtualOntology extends ObaOntology{
     public ObaClass getRoot() {
         // the root could also be retrieved directly from the ontology with
         // ontology.getOWLOntologyManager().getOWLDataFactory().getOWLThing()'
-        ObaOntology obOntA=ontA.getOntology();
-        return obOntA.getRoot();     
+        ObaOntology obOntA = ontA.getOntology();
+        return obOntA.getRoot();
     }
-    
-    
-    
+
     @Override
-        protected void scanClasses(OWLOntology ontology)
+    protected void scanClasses(OWLOntology ontology)
             throws CorruptIndexException, LockObtainFailedException,
             IOException {
-            orphanChildren.addAll(ontA.getOntology().getOrphanChildren());
-            orphanChildren.addAll(ontB.getOntology().getOrphanChildren());
-            obsoleteClasses.addAll(ontA.getOntology().getObsoleteClasses());
-            obsoleteClasses.addAll(ontA.getOntology().getObsoleteClasses());
+    }
+
+    public ArrayList exportClasses(OWLOntology ontology) {
+        ArrayList<OWLClass> outClasses = new ArrayList<OWLClass>();
+        Set<OWLDeclarationAxiom> classes = ontology.getAxioms(AxiomType.DECLARATION);
+        int counter = 0;
+        clsLoop:
+        for (OWLDeclarationAxiom c : classes) {
+
+            OWLEntity entity = c.getSignature().iterator().next();
+            if (!(entity instanceof OWLClass)) {
+                // skip AnnotationProperties, ObjectProperties, DataProperites
+                continue;
+            }
+            OWLClass cls = (OWLClass) entity;
+            outClasses.add(cls);
+            counter++;
         }
-   
-    
+        return outClasses;
+    }
 
 }
